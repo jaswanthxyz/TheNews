254bfe3d679c91dfa9598edd28b83c5a6cb96147
Jaswanth Manigundan <jaswanth.manigundan@transpire.com>
2021-06-30 01:37:42 +1000

    Initial Commit

---
+++ Shared/TheNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNewsApp.swift
+//  Shared
+//
+//  Created by Jaswanth Manigundan on 30/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TheNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
