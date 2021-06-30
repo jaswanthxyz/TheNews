cadd1fef015765790d090975e4ef42810afd9ec3
Jaswanth Manigundan <jaswanth.ooty@gmail.com>
2021-06-30 12:38:44 +1000

    More Unit Tests
    Updated unit tests for most views and models

--- Tests iOS/ViewModel/NewsViewModelTests.swift
+++ Tests iOS/ViewModel/NewsViewModelTests.swift
@@ -9,42 +9,85 @@ import XCTest
 
 class NewsViewModelTests: XCTestCase {
     
-    func testSuccessFetchData() {
+    let jsonString = """
+        {
+          "status": "ok",
+          "feed": {
+            "url": "http://www.abc.net.au/news/feed/51120/rss.xml",
+            "title": "Just In",
+            "link": "https://www.abc.net.au/news/justin/",
+            "author": "",
+            "description": "",
+            "image": "https://www.abc.net.au/news/image/8413416-1x1-144x144.png"
+          },
+          "items": [
+            {
+              "title": "Perth and Peel plunged into lockdown after another local coronavirus case detected",
+              "pubDate": "2021-06-28 12:13:05",
+              "link": "https://www.abc.net.au/news/2021-06-28/wa-coronavirus-perth-peel-hard-lockdown/100250920",
+              "guid": "https://www.abc.net.au/news/2021-06-28/wa-coronavirus-perth-peel-hard-lockdown/100250920",
+              "author": "",
+              "thumbnail": "https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=3333&amp;cropW=2500&amp;xPos=1578&amp;yPos=0&amp;width=862&amp;height=1149",
+              "description": "Western Australia records a further positive case of COVID-19 and effective from midnight, Perth and Peel will enter a four-day hard lockdown. ",
+              "content": "Western Australia records a further positive case of COVID-19 and effective from midnight, Perth and Peel will enter a four-day hard lockdown. ",
+              "enclosure": {
+                "link": "https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=2813&amp;cropW=5000&amp;xPos=0&amp;yPos=60&amp;width=862&amp;height=485",
+                "type": "image/jpeg",
+                "thumbnail": "https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=3333&amp;cropW=2500&amp;xPos=1578&amp;yPos=0&amp;width=862&amp;height=1149"
+              },
+              "categories": [
+                "COVID-19",
+                "State and Territory Government",
+                "Lockdown"
+              ]
+            },
+          ]
+        }
+        """
+    
+    func testSuccessFetchDataAndParsing() {
+        
         let decoder = JSONDecoder()
-        let jsonString = "{\n" +
-            "      \"title\": \"Perth and Peel plunged into lockdown after another local coronavirus case detected\",\n" +
-            "      \"pubDate\": \"2021-06-28 12:13:05\",\n" +
-            "      \"link\": \"https://www.abc.net.au/news/2021-06-28/wa-coronavirus-perth-peel-hard-lockdown/100250920\",\n" +
-            "      \"guid\": \"https://www.abc.net.au/news/2021-06-28/wa-coronavirus-perth-peel-hard-lockdown/100250920\",\n" +
-            "      \"author\": \"\",\n" +
-            "      \"thumbnail\": \"https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=3333&amp;cropW=2500&amp;xPos=1578&amp;yPos=0&amp;width=862&amp;height=1149\",\n" +
-            "      \"description\": \"Western Australia records a further positive case of COVID-19 and effective from midnight, Perth and Peel will enter a four-day hard lockdown. \",\n" +
-            "      \"content\": \"Western Australia records a further positive case of COVID-19 and effective from midnight, Perth and Peel will enter a four-day hard lockdown. \",\n" +
-            "      \"enclosure\": {\n" +
-            "        \"link\": \"https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=2813&amp;cropW=5000&amp;xPos=0&amp;yPos=60&amp;width=862&amp;height=485\",\n" +
-            "        \"type\": \"image/jpeg\",\n" +
-            "        \"thumbnail\": \"https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=3333&amp;cropW=2500&amp;xPos=1578&amp;yPos=0&amp;width=862&amp;height=1149\"\n" +
-            "      },\n" +
-            "      \"categories\": [\n" +
-            "        \"COVID-19\",\n" +
-            "        \"State and Territory Government\",\n" +
-            "        \"Lockdown\"\n" +
-            "      ]\n" +
-            "    }";
+        
         
         do {
-            let expectedList = try decoder.decode([Items].self, from: Data(jsonString.utf8))
-            let service = MockService(mockData: expectedList)
+            print("here")
+            let data = Data(jsonString.utf8)
+            let expectedList = try decoder.decode(NewsResponse.self, from: data)
+            let service = MockService(mockData: expectedList.items)
             
             let viewModel = NewsViewModel(service: service)
-            
             
             viewModel.loadNews()
             
             XCTAssertTrue(!viewModel.loading)
             XCTAssertEqual(viewModel.newsItems.items.count, 1)
+
         } catch {
             print(error.localizedDescription)
+            print(String(describing: error))
+            XCTFail(String(describing: error))
+        }
+    }
+    
+    func testSuccessParsing() {
+        
+        let decoder = JSONDecoder()
+        
+        
+        do {
+            let data = Data(jsonString.utf8)
+            let expectedList = try decoder.decode(NewsResponse.self, from: data)
+            XCTAssertEqual(expectedList.items!.count, 1)
+            XCTAssertEqual(expectedList.items![0].categories!.count, 3)
+            XCTAssertEqual(expectedList.feed!.link, "https://www.abc.net.au/news/justin/")
+            XCTAssertNotNil(expectedList.items![0].enclosure?.link)
+            
+
+        } catch {
+            print(error.localizedDescription)
+            print(String(describing: error))
+            XCTFail(String(describing: error))
         }
     }
     
---
+++ Tests iOS/Views/NewsCardTests.swift
@@ -0,0 +1,69 @@
+//
+//  NewsCardTests.swift
+//  TheNews
+//
+//  Created by Jaswanth Manigundan on 30/6/21.
+//
+
+import XCTest
+import ViewInspector
+
+
+class NewsCardTests: XCTestCase {
+    
+    func testNewsCard() throws {
+        
+        let decoder = JSONDecoder()
+        let jsonString = """
+            {
+              "status": "ok",
+              "feed": {
+                "url": "http://www.abc.net.au/news/feed/51120/rss.xml",
+                "title": "Just In",
+                "link": "https://www.abc.net.au/news/justin/",
+                "author": "",
+                "description": "",
+                "image": "https://www.abc.net.au/news/image/8413416-1x1-144x144.png"
+              },
+              "items": [
+                {
+                  "title": "Perth and Peel plunged into lockdown after another local coronavirus case detected",
+                  "pubDate": "2021-06-28 12:13:05",
+                  "link": "https://www.abc.net.au/news/2021-06-28/wa-coronavirus-perth-peel-hard-lockdown/100250920",
+                  "guid": "https://www.abc.net.au/news/2021-06-28/wa-coronavirus-perth-peel-hard-lockdown/100250920",
+                  "author": "",
+                  "thumbnail": "https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=3333&amp;cropW=2500&amp;xPos=1578&amp;yPos=0&amp;width=862&amp;height=1149",
+                  "description": "Western Australia records a further positive case of COVID-19 and effective from midnight, Perth and Peel will enter a four-day hard lockdown. ",
+                  "content": "Western Australia records a further positive case of COVID-19 and effective from midnight, Perth and Peel will enter a four-day hard lockdown. ",
+                  "enclosure": {
+                    "link": "https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=2813&amp;cropW=5000&amp;xPos=0&amp;yPos=60&amp;width=862&amp;height=485",
+                    "type": "image/jpeg",
+                    "thumbnail": "https://live-production.wcms.abc-cdn.net.au/9ac8c8dcf33c8024416b8a108d1768de?impolicy=wcms_crop_resize&amp;cropH=3333&amp;cropW=2500&amp;xPos=1578&amp;yPos=0&amp;width=862&amp;height=1149"
+                  },
+                  "categories": [
+                    "COVID-19",
+                    "State and Territory Government",
+                    "Lockdown"
+                  ]
+                },
+              ]
+            }
+            """
+        
+        do {
+            let data = Data(jsonString.utf8)
+            let expectedList = try decoder.decode(NewsResponse.self, from: data)
+            let subject = NewsCard(item: expectedList.items![0])
+            let text = try subject.inspect().hStack().vStack(0).text(0).string()
+            XCTAssertEqual(text, "Perth and Peel plunged into lockdown after another local coronavirus case detected")
+            
+        } catch {
+            print(error.localizedDescription)
+            print(String(describing: error))
+            XCTFail(String(describing: error))
+        }
+    }
+    
+}
+
+extension NewsCard: Inspectable {}
